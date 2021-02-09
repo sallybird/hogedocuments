429555350916172ef0372dc94079d713653f084b
Risa Watahiki <55229897+sallybird@users.noreply.github.com>
2021-02-09 21:40:19 +0900

    Update PageHeader.vue

--- docs/.vuepress/components/PageHeader.vue
+++ docs/.vuepress/components/PageHeader.vue
@@ -4,7 +4,6 @@
             <strong>Updated:</strong> {{ created }}<br />
             <strong>Created:</strong> {{ updated }}<br />
             <strong>Last Updated By:</strong> {{ author }}<br />
-            <strong>Read Time:</strong> {{ readingTime }} minute(s)<br />
             
         </p>
     </div>
@@ -13,19 +12,6 @@
 <script>
 export default {
     name: 'PageHeader',
-    computed: {
-        // Full object list: //github.com/darrenjennings/vuepress-plugin-reading-time#usage
-        readingTime() { 
-            let minutes = this.$page.readingTime.minutes
-            
-            if (minutes < 1) {
-                minutes = 1
-            } else {
-                minutes = (minutes - Math.floor(minutes) < .5) ? Math.floor(minutes) : Math.ceil(minutes)
-            }
-
-            return minutes 
-        },
         // Full list of API: //vuepress.github.io/en/plugins/git-log/#api
         author() { return this.$page.git.author; },
         commits() { return this.$page.git.commits; },
