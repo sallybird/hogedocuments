21bf475f9ec625312586f8a4b845bdd8cb42e278
Risa Watahiki <55229897+sallybird@users.noreply.github.com>
2021-02-09 20:22:17 +0900

    Update Page.vue

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -20,8 +20,8 @@
 
 <script>
 import Breadcrumbs from "@theme/components/Breadcrumbs.vue";
-import PageEdit from "@parent-theme/components/PageEdit.vue";
-import PageNav from "@parent-theme/components/PageNav.vue";
+import PageEdit from "@theme/components/PageEdit.vue";
+import PageNav from "@theme/components/PageNav.vue";
 
 export default {
   components: { Breadcrumbs, PageEdit, PageNav },
